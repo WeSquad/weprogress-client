86cc1f10df92c968c9f028c0961db9bbb5aff207
Nicolas <pinon.nico@gmail.com>
2019-04-29 17:26:28 +0200

    Compare assessments : add soft skills

--- src/components/MyAssessments/ViewComparedAssessments.js
+++ src/components/MyAssessments/ViewComparedAssessments.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Typography, Paper, FormHelperText } from '@material-ui/core';
+import { Typography, Paper, FormHelperText, Divider } from '@material-ui/core';
 import { fade } from '@material-ui/core/styles/colorManipulator';
 import { Radar} from 'react-chartjs-2';
 import { withStyles } from '@material-ui/core/styles';
@@ -21,22 +21,36 @@ const ASSESSMENT_RATES_QUERY = gql`
   }
 `;
 
+const ASSESSMENT_SOFTSKILLS_RATES_QUERY = gql`
+  query assessmentSoftSkillsRates($id: ID!){
+    assessmentSoftSkillsRates(id: $id) {
+      skillName
+      skillRate
+    }
+  }
+`;
+
 class ViewComparedAssessments extends Component {
   constructor(props) {
     super();
     this.state = {
       loadingQuery: true,
-      axesNames: [],
+      hardAxesNames: [],
+      softAxesNames: [],
       firstHardAxesValues: [],
       secondHardAxesValues: [],
+      firstSoftAxesValues: [],
+      secondSoftAxesValues: [],
       openJobDialog: false,
     };
 
-    this.fetchFirstAssessment(props);
-    this.fetchSecondAssessment(props);
+    this.fetchFirstHardAssessment(props);
+    this.fetchSecondHardAssessment(props);
+    this.fetchFirstSoftSkills(props);
+    this.fetchSecondSoftSkills(props);
   };
 
-  fetchFirstAssessment = async (props) => {
+  fetchFirstHardAssessment = async (props) => {
     const { client, firstId } = props;
     const { data } = await client.query({
       query: ASSESSMENT_RATES_QUERY,
@@ -55,13 +69,13 @@ class ViewComparedAssessments extends Component {
     });
 
     this.setState({
-      axesNames: names,
+      hardAxesNames: names,
       firstHardAxesValues: values,
       loadingQuery: false,
     });
   };
 
-  fetchSecondAssessment = async (props) => {
+  fetchSecondHardAssessment = async (props) => {
     const { client, secondId } = props;
     const { data } = await client.query({
       query: ASSESSMENT_RATES_QUERY,
@@ -83,37 +97,80 @@ class ViewComparedAssessments extends Component {
     });
   };
 
+  fetchFirstSoftSkills = async (props) => {
+    const { client, firstId } = props;
+    const { data } = await client.query({
+      query: ASSESSMENT_SOFTSKILLS_RATES_QUERY,
+      variables: { "id": firstId },
+      fetchPolicy: "no-cache"
+    });
+
+    var names = [];
+    var values = [];
+
+    data.assessmentSoftSkillsRates.forEach(rate => {
+      names.push(rate.skillName);
+      values.push(rate.skillRate);
+    });
+
+    this.setState({
+      softAxesNames: names,
+      firstSoftAxesValues: values,
+      loadingQuery: false,
+    });
+  };
+
+  fetchSecondSoftSkills = async (props) => {
+    const { client, secondId } = props;
+    const { data } = await client.query({
+      query: ASSESSMENT_SOFTSKILLS_RATES_QUERY,
+      variables: { "id": secondId },
+      fetchPolicy: "no-cache"
+    });
+
+    var values = [];
+
+    data.assessmentSoftSkillsRates.forEach(rate => {
+      values.push(rate.skillRate);
+    });
+
+    this.setState({
+      secondSoftAxesValues: values,
+      loadingQuery: false,
+    });
+  };
+
   render() {
     const { classes } = this.props;
-    const { axesNames, firstHardAxesValues, secondHardAxesValues, loadingQuery } = this.state;
+    const { hardAxesNames, softAxesNames, firstHardAxesValues, secondHardAxesValues, firstSoftAxesValues, secondSoftAxesValues, loadingQuery } = this.state;
 
-    const data = {
-      labels: axesNames,
+    const hardData = {
+      labels: hardAxesNames,
       datasets: [
         {
           label: "Assessment initial",
-          backgroundColor: fade(theme.palette.secondary.main, 0.2),
-          borderColor: theme.palette.secondary.main,
-          pointBackgroundColor: theme.palette.secondary.main,
-          pointBorderColor: theme.palette.secondary.light,
-          pointHoverBackgroundColor: theme.palette.secondary.dark,
-          pointHoverBorderColor: theme.palette.secondary.light,
-          data: firstHardAxesValues
-        },
-        {
-          label: "Second Assessment",
           backgroundColor: fade(theme.palette.primary.main, 0.2),
           borderColor: theme.palette.primary.main,
           pointBackgroundColor: theme.palette.primary.main,
           pointBorderColor: theme.palette.primary.light,
           pointHoverBackgroundColor: theme.palette.primary.dark,
           pointHoverBorderColor: theme.palette.primary.light,
+          data: firstHardAxesValues
+        },
+        {
+          label: "Second Assessment",
+          backgroundColor: fade(theme.palette.secondary.main, 0.2),
+          borderColor: theme.palette.secondary.main,
+          pointBackgroundColor: theme.palette.secondary.main,
+          pointBorderColor: theme.palette.secondary.light,
+          pointHoverBackgroundColor: theme.palette.secondary.dark,
+          pointHoverBorderColor: theme.palette.secondary.light,
           data: secondHardAxesValues,
         }
       ]
     };
 
-    const options = {
+    const hardOptions = {
       responsive: true,
       legend: {
         display: false,
@@ -139,6 +196,67 @@ class ViewComparedAssessments extends Component {
       }
     };
 
+    const softData = {
+      labels: softAxesNames,
+      datasets: [
+        {
+          label: "Assessment initial",
+          backgroundColor: fade(theme.palette.primary.main, 0.2),
+          borderColor: theme.palette.primary.main,
+          pointBackgroundColor: theme.palette.primary.main,
+          pointBorderColor: theme.palette.primary.light,
+          pointHoverBackgroundColor: theme.palette.primary.dark,
+          pointHoverBorderColor: theme.palette.primary.light,
+          data: firstSoftAxesValues,
+          lineTension: 0.5
+        },
+        {
+          label: "Second Assessment",
+          backgroundColor: fade(theme.palette.secondary.main, 0.2),
+          borderColor: theme.palette.secondary.main,
+          pointBackgroundColor: theme.palette.secondary.main,
+          pointBorderColor: theme.palette.secondary.light,
+          pointHoverBackgroundColor: theme.palette.secondary.dark,
+          pointHoverBorderColor: theme.palette.secondary.light,
+          data: secondSoftAxesValues,
+          lineTension: 0.5
+        }
+      ]
+    };
+
+    const softOptions = {
+      responsive: true,
+      legend: {
+        display: false,
+      },
+      scale: {
+        gridLines: {
+          circular: true,
+        },
+        angleLines: {
+          "color": "white",
+        },
+        ticks: {
+          beginAtZero: true,
+          stepSize: 1,
+          max: 5,
+          display: false,
+        },
+        pointLabels: {
+          display: false
+        }
+      },
+      maintainAspectRatio: false,
+      tooltips: {
+        callbacks: {
+          label: function(tooltipItem, data) {
+            var amount = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index];
+            return " " + amount + " / 5";
+          }
+        }
+      }
+    };
+
     return (
       <div>
         {loadingQuery? (
@@ -149,8 +267,22 @@ class ViewComparedAssessments extends Component {
               Comparatif de mes assessments
             </Typography>
             <Paper className={classes.paper}>
+            <div className={classes.skillTitle}>
+                <Typography variant="h6" component="h4" className={classes.skillType}>
+                  Hard Skills
+                </Typography>
+              </div>
               <div className={classes.canvasContainer}>
-                <Radar data={data} options={options} />
+                <Radar data={hardData} options={hardOptions} />
+              </div>
+              <Divider />
+              <div className={classes.skillTitle}>
+                <Typography variant="h6" component="h4" className={classes.skillType}>
+                  Soft Skills
+                </Typography>
+              </div>
+              <div className={classes.canvasContainer}>
+                <Radar data={softData} options={softOptions} />
               </div>
             </Paper>
           </div>
